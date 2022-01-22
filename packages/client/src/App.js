89e1699a9f13c74e55ec01e34d5df631244e190d
nualn <nuuttinikkola1@gmail.com>
2022-01-22 18:00:12 +0200

    Add base styling

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import LiveGames from './components/LiveGames';
 
 const App = () => {
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <LiveGames />
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <LiveGames/>
+      </Box>
+    </Container>
   );
 };
 
--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -4,10 +4,17 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './store';
 
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store} >
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
