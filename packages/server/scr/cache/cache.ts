3c8c99c78d7998e80a0febe38979b770872a9891
nualn <nuuttinikkola1@gmail.com>
2022-02-02 10:37:44 +0200

    Remove console.log

--- packages/server/scr/cache/cache.ts
+++ packages/server/scr/cache/cache.ts
@@ -73,8 +73,6 @@ const setPlayerGames = async (playerGamesObj: PlayerGames) => {
 
 const storeGames = async (playerGamesArray: Array<PlayerGames>) => {
   for (const playerGamesObj of playerGamesArray) {
-    console.time('Add playerGames');
-    console.log('storing', playerGamesObj.games.length, 'games for', playerGamesObj.name);
     const cachedPlayerGamesObj = await getPlayerGames(playerGamesObj.name);
     if (cachedPlayerGamesObj) {
       await setPlayerGames({ 
@@ -84,7 +82,6 @@ const storeGames = async (playerGamesArray: Array<PlayerGames>) => {
     } else {
       await setPlayerGames(playerGamesObj);
     }
-    console.timeEnd('Add playerGames');
   }
 };
 
